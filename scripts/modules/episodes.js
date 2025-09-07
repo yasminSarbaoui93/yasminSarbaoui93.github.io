5f27100aee3bc7e6e083effb7651630292966e25
yasminSarbaoui93 <56456235+yasminSarbaoui93@users.noreply.github.com>
2025-09-08 00:11:12 +0200

    Actualizar episodes.js

--- scripts/modules/episodes.js
+++ scripts/modules/episodes.js
@@ -30,4 +30,6 @@ export const EPISODES = [
   "https://soundcloud.com/sednafm/morning-drops-sicilians-on-the-dolomites",
   "https://soundcloud.com/sednafm/morning-drops-morning-dubs",
   "https://soundcloud.com/sednafm/morning-drops-the-rise-of-afrobeat"
+  "https://m.soundcloud.com/sednafm/morning-drops-a-trip-hop-journey"
+  "https://m.soundcloud.com/sednafm/morning-drops-canserbero"
 ];
