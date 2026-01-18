4210fcd58d4f5fe1584bbd01b891553c8f84a1fe
yasminSarbaoui93 <yasmin.sarbaoui@gmail.com>
2026-01-18 18:09:05 +0100

    feat: epiusode update

--- scripts/modules/episodes.js
+++ scripts/modules/episodes.js
@@ -50,5 +50,6 @@ export const EPISODES = [
   "https://soundcloud.com/sednafm/evening-flows-party-in-the-medina",
   "https://soundcloud.com/sednafm/morning-drops-the-joy-of-learning",
   "https://soundcloud.com/sednafm/morning-drops-looking-back",
-  "https://soundcloud.com/sednafm/morning-drops-residente"
+  "https://soundcloud.com/sednafm/morning-drops-residente",
+  "https://soundcloud.com/sednafm/morning-drops-it-cant-rain-forever"
 ];
